5aa72ef59e2fe9346aa1f01b362e1305ff44547a
Andrea Sierra <andrea.sierra05@uptc.edu.co>
2024-05-26 12:56:37 -0500

    add connection to mongodb

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,7 +1,9 @@
 # Create your models here.
 from django.db import models
+from db_conection import db
 
 # Create your models here.
+todo_coleccion = db['todo']
 
 class Todo(models.Model):
     title = models.CharField(max_length=120)
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -2,9 +2,29 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import TodoSerializer
 from .models import Todo
+from .models import todo_coleccion
+from django.http import HttpResponse
 
 # Create your views here.
 
+def index(request):
+    return HttpResponse("<h1>App is running</h1>")
+
+def add_task(request):
+    records = {
+        "id": "1",
+        "title": "task", 
+        "description": "add task", 
+        "completed": "false"
+    }
+    todo_coleccion.insert_one(records)
+    return HttpResponse("New task is added")
+
+
+def get_all_task(request):
+    task = todo_coleccion.find()
+    return HttpResponse(task)
+    
 class TodoView(viewsets.ModelViewSet):
     serializer_class = TodoSerializer
     queryset = Todo.objects.all()
